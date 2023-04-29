6e4000f9fc7a2fc6fb3cdb92a81c19a8669bd806
Denny Liang <denny.liang29@gmail.com>
2023-04-29 15:05:30 -0400

    webhook test

---
+++ pages/api/webhooks/typeform.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+
+const createServerClient = (req: NextApiRequest, res: NextApiResponse) => {
+  const supabase = createServerSupabaseClient(
+    { req, res },
+    {
+      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL || "",
+      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_PROJECT_KEY || "",
+    }
+  );
+  return supabase;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log(req.body);
+  console.log(req.query);
+  console.log(req.headers);
+}
